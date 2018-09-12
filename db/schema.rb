42ba2b2a34a04b235e39c20f66ace8ea1f6e9cb5
Kenny Cohen <kicohen@andrew.cmu.edu>
2018-09-11 23:45:15 -0400

    Changed to pgsql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_09_12_025554) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "subtitle"
